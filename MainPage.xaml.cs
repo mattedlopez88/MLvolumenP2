c192779e16c9fe06e585484f6ba7101d40b5076d
mattedlopez88 <matias.lopez@udla.edu.ec>
2024-12-02 10:21:39 -0500

    funcionalidades de conversion

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,20 +11,22 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            if (ML_litros != null) {
-                string ML_litrosString = ML_litros.Text;
-                float ML_litrosNumber = Single.Parse(ML_litrosString);
-
-                ML_gal.Text = (ML_litrosNumber * 0.264172).ToString();
-                ML_m3.Text = (ML_litrosNumber * 0.000001).ToString();
+            if (!string.IsNullOrWhiteSpace(ML_litros.Text) && float.TryParse(ML_litros.Text, out float ML_floatLitros))
+            {
+                ML_gal.Text = (ML_floatLitros * 0.264172).ToString();
+                ML_m3.Text = (ML_floatLitros * 0.219969).ToString();
             }
-            count++;
-
-            if (count == 1)
-                ML_Convert.Text = $"Clicked {ML_litros.Text} time";
-            else
-                ML_Convert.Text = $"Clicked {ML_litros.Text} times";
-
+            else if (!string.IsNullOrWhiteSpace(ML_gal.Text) && float.TryParse(ML_gal.Text, out float ML_floatGalones))
+            {
+                ML_litros.Text = (ML_floatGalones * 4.54609).ToString();
+                ML_m3.Text = (ML_floatGalones * 0.00454609).ToString();
+            }
+            else if (!string.IsNullOrWhiteSpace(ML_m3.Text) && float.TryParse(ML_m3.Text, out float ML_floatM3))
+            {
+                ML_litros.Text = (ML_floatM3 * 1000).ToString();
+                ML_gal.Text = (ML_floatM3 * 219.969).ToString();
+            }
+            
             SemanticScreenReader.Announce(ML_Convert.Text);
         }
 
