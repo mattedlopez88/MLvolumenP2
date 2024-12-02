eca8b59f551ca5b6d24d842adb2d8a8516a9cc62
mattedlopez88 <matias.lopez@udla.edu.ec>
2024-12-02 10:03:09 -0500

    declaracion de cuadros de texto y botones

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,14 +11,28 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
+            if (ML_litros != null) {
+                string ML_litrosString = ML_litros.Text;
+                float ML_litrosNumber = Single.Parse(ML_litrosString);
+
+                ML_gal.Text = (ML_litrosNumber * 0.264172).ToString();
+                ML_m3.Text = (ML_litrosNumber * 0.000001).ToString();
+            }
             count++;
 
             if (count == 1)
-                ML_Convert.Text = $"Clicked {count} time";
+                ML_Convert.Text = $"Clicked {ML_litros.Text} time";
             else
-                ML_Convert.Text = $"Clicked {count} times";
+                ML_Convert.Text = $"Clicked {ML_litros.Text} times";
 
             SemanticScreenReader.Announce(ML_Convert.Text);
+        }
+
+        private void OnBorrarClicked(object sender, EventArgs e)
+        {
+            ML_litros.Text = "";
+            ML_gal.Text = "";
+            ML_m3.Text = "";
         }
     }
 
